cf3b2d638cbfba910d6ae98142c02c24d8072fa0
unknown <mauricio_maletta@hotmail.com>
2022-07-05 03:25:41 -0300

    add references to slider component

---
+++ src/components/Slider/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+// import { Container } from './styles';
+
+// https://www.youtube.com/watch?v=2_E5uoiLCLY
+// https://www.youtube.com/watch?v=5bxFSOA5JYo
+// https://www.youtube.com/watch?v=3yfswsnD2sw
+
+//https://www.youtube.com/watch?v=C9EWifQ5xqA
+
+const Slider: React.FC = () => {
+  return <div />;
+};
+
+export default Slider;
