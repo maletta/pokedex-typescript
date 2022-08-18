296cb93c66553fde6602628825f41013b58e7155
unknown <mauricio_maletta@hotmail.com>
2022-08-17 21:50:19 -0300

    fix close modal with small height

--- src/components/ModalSlider/index.tsx
+++ src/components/ModalSlider/index.tsx
@@ -10,6 +10,7 @@ interface SliderProps {
 
 const ModalSlider: React.FC<SliderProps> = ({ children, isOpen, closeModal }) => {
   const sliderContainer = useRef<HTMLDivElement>(null);
+  const sliderContent = useRef<HTMLDivElement>(null);
   const [isGrabbed, setIsGrabbed] = useState<boolean>(false);
   const [currentTopOffset, setCurrentTopOffset] = useState<number>(0);
   const [cursorY, setCursorY] = useState<number>(0);
@@ -40,20 +41,17 @@ const ModalSlider: React.FC<SliderProps> = ({ children, isOpen, closeModal }) =>
 
   function onMouseUp() {
     setIsGrabbed(false);
-    // setCurrentTopOffset(sliderContent.current?.getClientRects()[0].top || 0);
     setCurrentTopOffset(bottom);
   }
 
   useEffect(() => {
-    // if offset is too small, so return to initial offset
-    if (!isGrabbed && sliderContainer.current && isOpen) {
-      const widthToBackToZero = -Math.floor(sliderContainer.current?.getClientRects()[0].height / 2);
-      console.log("sliderContainer height ", sliderContainer.current?.getClientRects()[0].height);
+    // if offset is too small, so return to initial offset, and dont close modal
+    // else close modal
+    if (!isGrabbed && sliderContent.current && isOpen) {
+      const widthToBackToZero = -Math.floor(sliderContent.current?.getClientRects()[0].height / 2);
       if (bottom > widthToBackToZero) {
-        console.log("bottom - widthTObackToZero - validation ", bottom, widthToBackToZero, bottom > widthToBackToZero);
         setBottom(0);
       } else {
-        console.log("fechar modal");
         closeModal();
         setCurrentTopOffset(0);
         setBottom(0);
@@ -85,7 +83,7 @@ const ModalSlider: React.FC<SliderProps> = ({ children, isOpen, closeModal }) =>
 
   return (
     <SliderContainer ref={sliderContainer} isOpen={isOpen} onMouseMove={onSliderContainerMouseMove}>
-      <SliderContent bottomOffset={bottom}>
+      <SliderContent ref={sliderContent} bottomOffset={bottom}>
         <DragBar onMouseDown={onDragBarMouseDown} />
         <SliderContentOverFlow>{children}</SliderContentOverFlow>
       </SliderContent>
