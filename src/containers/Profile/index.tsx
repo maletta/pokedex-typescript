b0a18676f2483069b2152e393b41c002118c6afc
Maletta <mauricio_maletta@hotmail.com>
2024-10-25 03:48:55 -0300

    fix: pokemon loading on pokemon profile page

--- src/containers/Profile/index.tsx
+++ src/containers/Profile/index.tsx
@@ -22,9 +22,12 @@ import {
   TabsContainer,
   TabsGroup,
   Tab,
+  PokemonNotFound,
 } from './styles';
 
 import { ReactComponent as BackIconSVG } from 'assets/back-icon.svg';
+import Loading from 'components/Loading';
+import Button from 'components/Button';
 
 enum TabsENUM {
   ABOUT = 'ABOUT',
@@ -39,6 +42,7 @@ const Profile: React.FC = () => {
   const [pokemon, setPokemon] = useState<IGetPokemon | null>(null);
   const [pokemonSpecies, setPokemonSpecies] = useState<IGetPokemonSpecies | null>(null);
   const [types, setTypes] = useState<Array<PokemonTypesKeyOf>>();
+  const [isLoading, setIsLoading] = useState(true);
   const navigate = useNavigate();
   const { pokemonId } = useParams();
 
@@ -57,7 +61,10 @@ const Profile: React.FC = () => {
       setTypes(typesAdapted);
 
       setPokemonSpecies(response[1].data);
-    });
+    })
+      .finally(() => {
+        setIsLoading(false);
+      });
     // getPokemon(String(pokemonId)).then(response => {
     //   setPokemon(response.data);
     //   const typesAdapted = pokemonTypesRequestAdapter(response.data.types);
@@ -66,60 +73,74 @@ const Profile: React.FC = () => {
     return () => window.removeEventListener('scroll', onScrollPage);
   }, [pokemonId]);
 
-  return pokemon && types && pokemonSpecies ? (
-    <ProfileWrapper type={types[0]}>
-      <Banner className={scrollY ? 'scrolled' : ''} type={pokemon.types[0].type.name.toUpperCase() as PokemonTypesKeyOf}>
-        <BackIconSVG className="backIcon" onClick={() => navigate('/')} />
-        <PokemonNameBackground className={scrollY ? 'scrolled' : ''} type={types[0]}>
-          {pokemon.name.toUpperCase()}
-        </PokemonNameBackground>
-        <PokemonInfo className={scrollY ? 'scrolled' : ''}>
-          <PokemonImage>
-            <img src={pokemon.sprites.other['official-artwork'].front_default} alt="pokemon illustrate" />
-          </PokemonImage>
+  return <>
+    {
+      pokemon && types && pokemonSpecies ? (
+        <ProfileWrapper type={types[0]}>
+          <Banner className={scrollY ? 'scrolled' : ''} type={pokemon.types[0].type.name.toUpperCase() as PokemonTypesKeyOf}>
+            <BackIconSVG className="backIcon" onClick={() => navigate('/')} />
+            <PokemonNameBackground className={scrollY ? 'scrolled' : ''} type={types[0]}>
+              {pokemon.name.toUpperCase()}
+            </PokemonNameBackground>
+            <PokemonInfo className={scrollY ? 'scrolled' : ''}>
+              <PokemonImage>
+                <img src={pokemon.sprites.other['official-artwork'].front_default} alt="pokemon illustrate" />
+              </PokemonImage>
 
-          <PokemonDescription>
-            <PokemonNumber variant="primary" customCss={{ fontSize: '1.6rem', opacity: 0.6 }}>
-              {fillPokemonNumber(pokemon.id)}
-            </PokemonNumber>
-            <PokemonName variant="secondary" customCss={{ fontSize: '3.2rem', textTransform: 'capitalize' }}>
-              {pokemon.name}
-            </PokemonName>
-            <BadgeGroup>
-              {types.map(itemType => (
-                <Badge key={itemType} type={itemType} />
-              ))}
-            </BadgeGroup>
-          </PokemonDescription>
-        </PokemonInfo>
-        <PokemonInfoScrolled className={!scrollY ? 'scrolled' : ''}>
-          <PokemonName variant="secondary" customCss={{ fontSize: '2.6rem', textTransform: 'capitalize' }}>
-            {pokemon.name}
-          </PokemonName>
-        </PokemonInfoScrolled>
-      </Banner>
-      <TabsContainer className={scrollY ? 'scrolled' : ''} type={types[0]}>
-        <TabsGroup>
-          <Tab onClick={() => setSelectedTab(TabsENUM.ABOUT)} className={selectedTab === TabsENUM.ABOUT ? 'active' : ''}>
-            About
-          </Tab>
-          <Tab onClick={() => setSelectedTab(TabsENUM.STATS)} className={selectedTab === TabsENUM.STATS ? 'active' : ''}>
-            Stats
-          </Tab>
-          <Tab onClick={() => setSelectedTab(TabsENUM.EVOLUTION)} className={selectedTab === TabsENUM.EVOLUTION ? 'active' : ''}>
-            Evolution
-          </Tab>
-        </TabsGroup>
-      </TabsContainer>
-      <Content className={scrollY ? 'scrolled' : ''}>
-        <About isOpen={selectedTab === TabsENUM.ABOUT} types={types} pokemonSpecies={pokemonSpecies} />
-        <Stats isOpen={selectedTab === TabsENUM.STATS} types={types} pokemonStats={pokemon.stats} name={pokemon.name} />
-        <Evolution isOpen={selectedTab === TabsENUM.EVOLUTION} type={types[0]} pokemonSpecies={pokemonSpecies} />
-      </Content>
-    </ProfileWrapper>
-  ) : (
-    <div>Pokemon Not Found</div>
-  );
+              <PokemonDescription>
+                <PokemonNumber variant="primary" customCss={{ fontSize: '1.6rem', opacity: 0.6 }}>
+                  {fillPokemonNumber(pokemon.id)}
+                </PokemonNumber>
+                <PokemonName variant="secondary" customCss={{ fontSize: '3.2rem', textTransform: 'capitalize' }}>
+                  {pokemon.name}
+                </PokemonName>
+                <BadgeGroup>
+                  {types.map(itemType => (
+                    <Badge key={itemType} type={itemType} />
+                  ))}
+                </BadgeGroup>
+              </PokemonDescription>
+            </PokemonInfo>
+            <PokemonInfoScrolled className={!scrollY ? 'scrolled' : ''}>
+              <PokemonName variant="secondary" customCss={{ fontSize: '2.6rem', textTransform: 'capitalize' }}>
+                {pokemon.name}
+              </PokemonName>
+            </PokemonInfoScrolled>
+          </Banner>
+          <TabsContainer className={scrollY ? 'scrolled' : ''} type={types[0]}>
+            <TabsGroup>
+              <Tab onClick={() => setSelectedTab(TabsENUM.ABOUT)} className={selectedTab === TabsENUM.ABOUT ? 'active' : ''}>
+                About
+              </Tab>
+              <Tab onClick={() => setSelectedTab(TabsENUM.STATS)} className={selectedTab === TabsENUM.STATS ? 'active' : ''}>
+                Stats
+              </Tab>
+              <Tab onClick={() => setSelectedTab(TabsENUM.EVOLUTION)} className={selectedTab === TabsENUM.EVOLUTION ? 'active' : ''}>
+                Evolution
+              </Tab>
+            </TabsGroup>
+          </TabsContainer>
+          <Content className={scrollY ? 'scrolled' : ''}>
+            <About isOpen={selectedTab === TabsENUM.ABOUT} types={types} pokemonSpecies={pokemonSpecies} />
+            <Stats isOpen={selectedTab === TabsENUM.STATS} types={types} pokemonStats={pokemon.stats} name={pokemon.name} />
+            <Evolution isOpen={selectedTab === TabsENUM.EVOLUTION} type={types[0]} pokemonSpecies={pokemonSpecies} />
+          </Content>
+        </ProfileWrapper>
+      ) : (
+        <PokemonNotFound type={types}>
+          {
+            !isLoading && (
+              <>
+                <p>Pokemon Not Found. </p>
+                <Button handleClick={() => navigate("/")}>Try another pokemon</Button>
+              </>
+            )
+          }
+        </PokemonNotFound >
+      )
+    }
+    <Loading isLoading={isLoading} />
+  </>
 };
 
 export default Profile;
